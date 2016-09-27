fd0cbbf80a3e2be5d8ab23805bee17f1dae9fec7
Nicolas Feigenbaum <feigenbaum.nicolas@gmail.com>
2016-09-26 23:42:23 -0700

    add constant module

---
+++ lib/constant.rb
@@ -0,0 +1,7 @@
+module Constant
+  ACCESS_TOKEN_HEADER  = 'X-AUTH-HEADER'
+  DEFAULT_API_BASE_URL = 'api.polyseer.io/polyseer'
+  DEFAULT_API_PROTOCOL = 'http://'
+  DEFAULT_API_VERSION  = 'v1'
+  DEFAULT_ENV          = 'RAILS_ENV'
+end
---
+++ test/unit/constant_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../../lib/constant.rb'
+
+RSpec.describe Constant do
+  it 'defines ACCESS_TOKEN_HEADER' do
+    expect(Constant::ACCESS_TOKEN_HEADER).to eq('X-AUTH-HEADER')
+  end
+
+  it 'defines DEFAULT_API_BASE_URL' do
+    expect(Constant::DEFAULT_API_BASE_URL).to eq('api.polyseer.io/polyseer')
+  end
+
+  it 'defines DEFAULT_API_PROTOCOL' do
+    expect(Constant::DEFAULT_API_PROTOCOL).to eq('http://')
+  end
+
+  it 'defines DEFAULT_API_VERSION' do
+    expect(Constant::DEFAULT_API_VERSION).to eq('v1')
+  end
+
+  it 'defines DEFAULT_ENV' do
+    expect(Constant::DEFAULT_ENV).to eq('RAILS_ENV')
+  end
+end
