e31ff204d7167650fb949eae0751607b73d5064f
Nicolas Feigenbaum <feigenbaum.nicolas@gmail.com>
2016-09-27 12:57:05 -0700

    complete url_builder

--- lib/url_builder.rb
+++ lib/url_builder.rb
@@ -1,9 +1,11 @@
 require 'functional'
-require_relative 'resource'
+require 'constant'
+require 'resource'
 
 module UrlBuilder
   include Functional::Memo
 
+  # Resources that are mapped to environments.
   RoutableResources = [
     Resource::ALERT,
     Resource::EVENT,
@@ -12,23 +14,37 @@ module UrlBuilder
     Resource::TASK
   ]
 
+  # Determine if a resource is routable to an environment.
   def self.is_routable_resource(resource)
     RoutableResources.include? resource
   end
 
+  # Generate a base API url.
+  def self.get_base_url(
+    base     = Constant::DEFAULT_API_BASE_URL, 
+    protocol = Constant::DEFAULT_API_PROTOCOL, 
+    version  = Constant::DEFAULT_API_VERSION
+  )
+    "#{protocol}#{base}/#{version}"
+  end
+
+  # Get a resource's path.
   def self.get_resource_path(resource, options = {})
     environment = ''
 
     if is_routable_resource resource
-      if true
-
+      if !options.include? :eid
+        raise ArgumentError, "Cannot get routable resource path for #{resource} without passing an eid to options."
       end
 
-      environment = "environments/%{eid}" % options
+      environment = "environments/%{eid}/" % options
     end
 
-    "/%{environment}%{resource}${id}"
+    id = (options.include? :id) ? ("/%{id}" % options) : '';
+
+    "/#{environment}#{resource}#{id}"
   end
 
   memoize(:is_routable_resource)
+  memoize(:get_resource_path)
 end
--- test/unit/url_builder_spec.rb
+++ test/unit/url_builder_spec.rb
@@ -15,6 +15,77 @@ RSpec.describe UrlBuilder do
     end
   end
 
+  describe 'get_resource_path' do
+    it 'raises when routable resource without an eid' do
+      resource = 'alerts'
+      options = {}
+
+      expect { UrlBuilder::get_resource_path(resource, options) }. to raise_error(ArgumentError, /for alerts without passing/);
+    end
+
+    it 'returns the correct path for non-routable resources' do
+      resource = 'environments'
+      options = {}
+
+      result = UrlBuilder::get_resource_path(resource, options);
+
+      expect(result).to eq('/environments')
+    end
+
+    it 'returns the correct path for non-routable resources with an id' do
+      resource = 'environments'
+      options = { id: 3 }
+
+      result = UrlBuilder::get_resource_path(resource, options);
+
+      expect(result).to eq('/environments/3')
+    end
+
+    it 'returns the correct path for routable resources' do
+      resource = 'alerts'
+      options = { eid: 'test' }
+
+      result = UrlBuilder::get_resource_path(resource, options);
+
+      expect(result).to eq('/environments/test/alerts')
+    end
+
+    it 'returns the correct path for routable resources with an id' do
+      resource = 'alerts'
+      options = { eid: 'test', id: 3 }
+
+      result = UrlBuilder::get_resource_path(resource, options);
+
+      expect(result).to eq('/environments/test/alerts/3')
+    end
+  end
+
+  describe 'get_base_url' do
+    it 'has the correct default value' do
+      result = UrlBuilder::get_base_url();
+
+      expect(result).to eq('http://api.polyseer.io/polyseer/v1')
+    end
+
+    it 'can override base' do
+      result = UrlBuilder::get_base_url('ping.pong.io');
+
+      expect(result).to eq('http://ping.pong.io/v1')
+    end
+
+    it 'can override protocol' do
+      result = UrlBuilder::get_base_url('alpha.io', 'ws://');
+
+      expect(result).to eq('ws://alpha.io/v1')
+    end
+
+    it 'can override version' do
+      result = UrlBuilder::get_base_url('alpha.io', 'ws://', 'v2');
+
+      expect(result).to eq('ws://alpha.io/v2')
+    end
+  end
+
   describe 'is_routable_resource' do
     it 'returns true for routable resource' do
       result = UrlBuilder::is_routable_resource('alerts');
