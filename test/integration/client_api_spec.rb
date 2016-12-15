976d7f6482230215dbb47a583f27c5523060c9a7
Nicolas Feigenbaum <feigenbaum.nicolas@gmail.com>
2016-12-14 18:32:35 -0800

    add more client api integration tests
    add tests to ensure that the correct api methods
    are attached to the correct resources

--- test/integration/client_api_spec.rb
+++ test/integration/client_api_spec.rb
@@ -34,10 +34,6 @@ RSpec.describe Polyseerio do
     expect(client).to respond_to(:Alert)
   end
 
-  it 'contains Alert' do
-    expect(client).to respond_to(:Alert)
-  end
-
   it 'contains Channel' do
     expect(client).to respond_to(:Channel)
   end
@@ -72,5 +68,165 @@ RSpec.describe Polyseerio do
 
   it 'contains Task' do
     expect(client).to respond_to(:Task)
+  end
+
+  describe 'client.Alert' do
+    let(:resource) { client.Alert }
+    let(:instance) { resource.new {} }
+
+    it 'has the correct static API' do
+      expect(resource).to respond_to(:create)
+      expect(resource).to respond_to(:find)
+      expect(resource).to respond_to(:find_by_id)
+      expect(resource).to respond_to(:new)
+      expect(resource).to respond_to(:remove)
+      expect(resource).to respond_to(:trigger)
+      expect(resource).to respond_to(:update)
+    end
+
+    it 'has the correct instance API' do
+      expect(instance).to respond_to(:save)
+      expect(instance).to respond_to(:remove)
+      expect(instance).to respond_to(:trigger)
+    end
+  end
+
+  describe 'client.Channel' do
+    let(:resource) { client.Channel }
+    let(:instance) { resource.new {} }
+
+    it 'has the correct static API' do
+      expect(resource).to respond_to(:create)
+      expect(resource).to respond_to(:find)
+      expect(resource).to respond_to(:find_by_id)
+      expect(resource).to respond_to(:message)
+      expect(resource).to respond_to(:new)
+      expect(resource).to respond_to(:remove)
+      expect(resource).to respond_to(:update)
+    end
+
+    it 'has the correct instance API' do
+      expect(instance).to respond_to(:save)
+      expect(instance).to respond_to(:remove)
+      expect(instance).to respond_to(:message)
+    end
+  end
+
+  describe 'client.Environment' do
+    let(:resource) { client.Environment }
+    let(:instance) { resource.new {} }
+
+    it 'has the correct static API' do
+      expect(resource).to respond_to(:create)
+      expect(resource).to respond_to(:find)
+      expect(resource).to respond_to(:find_by_id)
+      expect(resource).to respond_to(:message)
+      expect(resource).to respond_to(:new)
+      expect(resource).to respond_to(:remove)
+      expect(resource).to respond_to(:update)
+    end
+
+    it 'has the correct instance API' do
+      expect(instance).to respond_to(:save)
+      expect(instance).to respond_to(:remove)
+      expect(instance).to respond_to(:message)
+    end
+  end
+
+  describe 'client.Event' do
+    let(:resource) { client.Event }
+    let(:instance) { resource.new {} }
+
+    it 'has the correct static API' do
+      expect(resource).to respond_to(:create)
+      expect(resource).to respond_to(:find)
+      expect(resource).to respond_to(:find_by_id)
+      expect(resource).to respond_to(:new)
+    end
+
+    it 'has the correct instance API' do
+      expect(instance).to respond_to(:save)
+    end
+  end
+
+  describe 'client.Expectation' do
+    let(:resource) { client.Expectation }
+    let(:instance) { resource.new {} }
+
+    it 'has the correct static API' do
+      expect(resource).to respond_to(:check)
+      expect(resource).to respond_to(:create)
+      expect(resource).to respond_to(:find)
+      expect(resource).to respond_to(:find_by_id)
+      expect(resource).to respond_to(:new)
+      expect(resource).to respond_to(:remove)
+      expect(resource).to respond_to(:update)
+    end
+
+    it 'has the correct instance API' do
+      expect(instance).to respond_to(:save)
+      expect(instance).to respond_to(:remove)
+      expect(instance).to respond_to(:check)
+    end
+  end
+
+  describe 'client.Instance' do
+    let(:resource) { client.Instance }
+    let(:instance) { resource.new {} }
+
+    it 'has the correct static API' do
+      expect(resource).to respond_to(:attach)
+      expect(resource).to respond_to(:create)
+      expect(resource).to respond_to(:find)
+      expect(resource).to respond_to(:find_by_id)
+      expect(resource).to respond_to(:new)
+      expect(resource).to respond_to(:remove)
+      expect(resource).to respond_to(:update)
+    end
+
+    it 'has the correct instance API' do
+      expect(instance).to respond_to(:save)
+      expect(instance).to respond_to(:remove)
+      expect(instance).to respond_to(:attach)
+      expect(instance).to respond_to(:detach)
+    end
+  end
+
+  describe 'client.Member' do
+    let(:resource) { client.Member }
+    let(:instance) { resource.new {} }
+
+    it 'has the correct static API' do
+      expect(resource).to respond_to(:create)
+      expect(resource).to respond_to(:find)
+      expect(resource).to respond_to(:find_by_id)
+      expect(resource).to respond_to(:new)
+      expect(resource).to respond_to(:remove)
+      expect(resource).to respond_to(:update)
+    end
+
+    it 'has the correct instance API' do
+      expect(instance).to respond_to(:save)
+      expect(instance).to respond_to(:remove)
+    end
+  end
+
+  describe 'client.Task' do
+    let(:resource) { client.Task }
+    let(:instance) { resource.new {} }
+
+    it 'has the correct static API' do
+      expect(resource).to respond_to(:create)
+      expect(resource).to respond_to(:find)
+      expect(resource).to respond_to(:find_by_id)
+      expect(resource).to respond_to(:new)
+      expect(resource).to respond_to(:remove)
+      expect(resource).to respond_to(:update)
+    end
+
+    it 'has the correct instance API' do
+      expect(instance).to respond_to(:save)
+      expect(instance).to respond_to(:remove)
+    end
   end
 end
