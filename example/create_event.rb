ec480ffa625b1cafea184a2d8a89e950f9dc86cf
Nicolas Feigenbaum <feigenbaum.nicolas@gmail.com>
2017-01-03 13:05:39 -0800

    improve example

--- example/create_event.rb
+++ example/create_event.rb
@@ -1,25 +1,43 @@
 require 'polyseerio'
+
 include Polyseerio::Enum
 
-# Setup a client that sends resources to the development environment.
+# First we will Create a client thats default behavior is to place resources 
+# the development environment.
 client = Polyseerio.make environment: 'development'
 
-# Create an event to indicate success.
-success_event = client.Event.create(
-  name: 'Success',
+# Direct SDK calls always return a Promise.
+#
+# Promises can be chained together to create an operation and then executed.
+# Be sure to handle the error cases of your promises correctly to avoid
+# any unexpected behavior.
+client.Event.create(
+  name: 'Success, something went right.',
   color: Color::GREEN,
   icon: Icon::CHECK
-).execute.value
+).then do |event|
+  puts "Success event #{event.id} has been created."
+end.execute.rescue do |error|
+  puts "Something went wrong while creating a success event."
+  puts error
+end.value
 
-# Create a danger event promise.
+# Like before we can create a promise.
+#
+# This time however, we will not execute the promise right away but rather
+# defer it until a bit later.
 emit_danger_event = client.Event.create(
-  name: 'Danger, something is wrong.',
+  name: 'Danger, something went wrong.',
   color: Color::RED,
   icon: Icon::ERROR
 )
 
-# Execute the promise.
-danger_event = emit_danger_event.execute.value
+# do some work...
 
-puts "Danger event #{danger_event.id} has been created."
-puts "Event #{success_event.id} has been created."
+# Not let's execute the danger event promise.
+danger_event = emit_danger_event.then do |event|
+  puts "Danger event #{event.id} has been created."
+end.execute.rescue do |error|
+  puts "Something went wrong while creating a danger event."
+  puts error
+end.value
